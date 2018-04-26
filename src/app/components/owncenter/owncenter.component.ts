fa39d622911f275ea26e9e5da8e81bc99669fa08
ifmake <1176033349@qq.com>
2018-04-26 18:19:25 +0800

    webpack

--- src/app/components/owncenter/owncenter.component.ts
+++ src/app/components/owncenter/owncenter.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { OwnCenterService } from '../../service/owncenter/owncenter.service';
 import { Config } from 'protractor/built/config';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-owncenter',
   templateUrl: './owncenter.component.html',
   styleUrls: ['./owncenter.component.scss'],
   providers: [OwnCenterService]
-
 })
 export class OwncenterComponent implements OnInit {
   userInfo: any;
+  UserFormInfo = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2)
+      ])
+    });
 
   constructor(private owninfo: OwnCenterService) { }
 
   ngOnInit() {
+    console.log(this.UserFormInfo);
     this.getOppointUserInfo();
   }
 
@@ -22,7 +29,12 @@ export class OwncenterComponent implements OnInit {
   getOppointUserInfo() {
     this.owninfo.getCurrentUserInfo(100011120).subscribe((data: Config) => {
       this.userInfo = data;
+      console.log(data);
     });
+  }
+  // 输入用户名
+  printUserName() {
+    console.log('sdfsdfsd');
   }
 
 }
--- src/app/components/owncenter/owncenter.module.ts
+++ src/app/components/owncenter/owncenter.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OwncenterComponent } from './owncenter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+// component
+import { OwncenterComponent } from './owncenter.component';
+// material
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
     RouterModule.forChild([
       {path: '', component: OwncenterComponent}
     ])
