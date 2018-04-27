6e3d05a46c0877834f708f40649edc84548761e8
ifmake <1176033349@qq.com>
2018-04-27 18:00:38 +0800

    activeform

--- src/app/components/owncenter/owncenter.component.ts
+++ src/app/components/owncenter/owncenter.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { OwnCenterService } from '../../service/owncenter/owncenter.service';
 import { Config } from 'protractor/built/config';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-owncenter',
@@ -9,27 +9,31 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
   styleUrls: ['./owncenter.component.scss'],
   providers: [OwnCenterService]
 })
-export class OwncenterComponent implements OnInit {
+export class OwncenterComponent implements OnInit, OnChanges {
   userInfo: any;
-  UserFormInfo = new FormGroup({
-      name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2)
-      ])
+  UserFormInfo: FormGroup;
+  constructor(private owninfo: OwnCenterService, private fb: FormBuilder) {
+    // 动态响应表单
+    this.UserFormInfo = this.fb.group({
+      name: ['', Validators.required],
+      remark: ['']
     });
-
-  constructor(private owninfo: OwnCenterService) { }
-
+  }
   ngOnInit() {
     console.log(this.UserFormInfo);
     this.getOppointUserInfo();
   }
+  ngOnChanges() {}
 
   // 获取指定用户的信息
   getOppointUserInfo() {
     this.owninfo.getCurrentUserInfo(100011120).subscribe((data: Config) => {
-      this.userInfo = data;
       console.log(data);
+      this.UserFormInfo.patchValue({
+          name: data.cnname,
+          remark: data.fullroute
+      });
+      this.userInfo = data;
     });
   }
   // 输入用户名
